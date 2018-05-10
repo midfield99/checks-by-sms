eb4d0bb8ecb4278f2423835f3e68c25ba3ab6c34
Ubuntu <midfield_3@hotmail.com>
2018-05-10 01:22:31 +0000

    app.py runs in debug mode by default now.

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def send_check(sms_check):
                         json=sms_check.checkbook_post_data())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug=True)
